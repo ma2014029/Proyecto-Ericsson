6bcc1e720abf5e8f091bc6ac925bb5bfaa664e93
JoseAdolfo132002 <adolfoaldanachigna@gmail.com>
2019-11-08 11:00:23 -0600

    DEVELOPING PARALLAX

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ import 'bootstrap';
 import css from 'css/bootstrap.min.css';
 import 'scss/main';
 import 'js/menu';
+import 'materialize-css/dist/js/materialize.js';
+
 $('.carousel').carousel();
+//Parallax Init
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+});
+
